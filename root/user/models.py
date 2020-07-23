382e410714ae1cbce1e3690ae2adf0a428692919
anowar143 <mdanowarhossain.s.bd@gmail.com>
2020-07-23 23:23:02 +0600

    user update

--- root/user/models.py
+++ root/user/models.py
@@ -64,10 +64,6 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
 
-    def has_perm(self, perm, obj=None): return self.is_superuser
-
-    def has_module_perms(self, app_label): return self.is_superuser
-
     class Meta:
         db_table = 'users'
         verbose_name = 'User'
